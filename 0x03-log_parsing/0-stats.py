7407e373e03bdefbb6b4d625e7f76c7eb2840108
MercyKalondu <mercykalondu62@gmail.com>
2023-04-13 12:11:52 +0000

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,43 +1,47 @@
 #!/usr/bin/python3
-""" A script that reads stdin line by line and computes metrics """
+""" script that reads stdin line by line and computes metrics """
 
-import sys
+if __name__ == '__main__':
 
+    import sys
 
-def print_status(dict, size):
-    """Print the format"""
-    print("File size: {}".format(size))
-    for key in sorted(dict.keys()):
-        if dict[key] != 0:
-            print("{}: {}".format(key, dict[key]))
+    def print_results(statusCodes, fileSize):
+        """ Print statistics """
+        print("File size: {:d}".format(fileSize))
+        for statusCode, times in sorted(statusCodes.items()):
+            if times:
+                print("{:s}: {:d}".format(statusCode, times))
 
+    statusCodes = {"200": 0,
+                   "301": 0,
+                   "400": 0,
+                   "401": 0,
+                   "403": 0,
+                   "404": 0,
+                   "405": 0,
+                   "500": 0
+                   }
+    fileSize = 0
+    n_lines = 0
 
-status_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,'404': 0, '405': 0, '500': 0}
-
-file_size = 0
-count = 0
-
-try:
-    for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            print_status(status_dict, file_size)
-
-        el = line.split(" ")
-        count += 1
-
-        try:
-            file_size += int(el[-1])
-        except:
-            pass
-
-        try:
-            if el[-2] in status_dict.keys():
-                status_dict[el[-2]] += 1
-        except:
-            pass
-        print_status(status_dict, file_size)
-
-
-except KeyboardInterrupt:
-    print_status(status_dict, file_size)
-    raise
+    try:
+        """ Read stdin line by line """
+        for line in sys.stdin:
+            if n_lines != 0 and n_lines % 10 == 0:
+                """ After every 10 lines, print from the beginning """
+                print_results(statusCodes, fileSize)
+            n_lines += 1
+            data = line.split()
+            try:
+                """ Compute metrics """
+                statusCode = data[-2]
+                if statusCode in statusCodes:
+                    statusCodes[statusCode] += 1
+                fileSize += int(data[-1])
+            except:
+                pass
+        print_results(statusCodes, fileSize)
+    except KeyboardInterrupt:
+        """ Keyboard interruption, print from the beginning """
+        print_results(statusCodes, fileSize)
+        raise
